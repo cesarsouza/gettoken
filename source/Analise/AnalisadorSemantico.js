ebd55dc6227f07b03854b100c5ccf87ae77d485f
leotaba <leotaba@26d602f2-abd2-11de-9787-55e617d4dd53>
2009-10-29 19:28:57 +0000

    Adicionando a classe AnalisadorSemantico!

---
+++ source/Analise/AnalisadorSemantico.js
@@ -0,0 +1,12 @@
+// Copyright © 2009 César Roberto de Souza, Leonardo Sameshima Taba
+// ----------------------------------------------------------------
+
+
+// Classe Analisador Semântico
+//  Esta classe é responsável por executar a etapa de análise semântica,
+//  analisando a corretude na utilização de variáveis e reportando os
+//  erros encontrados através da lista de erros em error()
+
+function AnalisadorSemantico() {
+
+}
